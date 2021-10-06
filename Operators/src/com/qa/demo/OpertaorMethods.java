f95f8394a631165982a2e8b2ac9f46e800b0f274
unknown <junathanabraham@live.co.uk>
2021-10-06 12:29:51 +0100

    operators tutorial

---
+++ Operators/src/com/qa/demo/OpertaorMethods.java
@@ -0,0 +1,23 @@
+package com.qa.demo;
+
+public class OpertaorMethods {
+	public static void addNumbers() {
+		int num1 = 6;
+		int num2 = 17;
+		int result = num1 + num2;
+		System.out.println(result);
+	}
+	
+	public static void minusNumbers() {
+		int num1 = 27;
+		int num2 = 17;
+		int result = num1 - num2;
+		System.out.println(result);
+	}
+	
+	public static int addTwoNums(int num1, int num2) {
+		return num1 + num2;
+		
+	}
+}
+
---
+++ Operators/src/com/qa/operator/Runner.java
@@ -0,0 +1,13 @@
+package com.qa.operator;
+
+import com.qa.demo.OpertaorMethods;
+
+public class Runner {
+	public static void main(String[] args) {
+		OpertaorMethods.addNumbers();
+		OpertaorMethods.minusNumbers();
+		System.out.println(OpertaorMethods.addTwoNums(7, 68));
+		
+	}
+
+}

52ba700a510c83ff794a62e2ab6de54cfa98ad64
unknown <junathanabraham@live.co.uk>
2021-10-07 11:48:00 +0100

    Dice Roller

---
+++ DiceRoll/src/com/qa/demo/DiceRoller.java
@@ -0,0 +1,34 @@
+package com.qa.demo;
+
+import java.util.Random;
+
+public class DiceRoller {
+	
+
+	
+	public static int fourDieOne(int num1) {
+		return new Random().nextInt(num1);
+		
+	}
+	
+	
+	public static int fourDieTwo(int num2) {
+		return new Random().nextInt(num2);
+		
+	}
+	
+	public static int fourDieThree(int num3) {
+		return new Random().nextInt(num3);
+		
+	}
+	
+	public static int fourDieFour(int num4) {
+		return new Random().nextInt(num4);//		
+	
+	}
+	
+
+	
+   
+
+}
---
+++ DiceRoll/src/com/qa/demo/Runner.java
@@ -0,0 +1,9 @@
+package com.qa.demo;
+
+public class Runner {
+public static void main(String[] args) {
+//	System.out.println(DiceRoller.randNum(6));
+	System.out.println(DiceRoller.fourDieOne(6) + DiceRoller.fourDieTwo(6) + DiceRoller.fourDieThree(6)+ DiceRoller.fourDieFour(6));
+	
+}
+}

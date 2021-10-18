6850835fb5607b5fc6151a9c75e4973f191f939f
unknown <junathanabraham@live.co.uk>
2021-10-18 16:14:26 +0100

    switch statement calcluator

--- Calculator/src/com/qa/calculator/CalcMethods.java
+++ Calculator/src/com/qa/calculator/CalcMethods.java
@@ -1,25 +1,25 @@
 package com.qa.calculator;
 
 public class CalcMethods {
-	public static void main(String[] args) {
+	public void main(String[] args) {
+	 
+	}
+	
+	public static int addcalc(int cal1, int cal2 ) {
+		return cal1 + cal2 ;
+		
+	}
+	public static int minuscalc(int cal1, int cal2) {
+		return cal1 - cal2;
 		
 	}
 	
-	public static int addcalc(int cal1, int cal2, int cal3) {
-		return cal1 + cal2 + cal3;
-		
-	}
-	public static int minuscalc(int cal1, int cal2,  int cal3) {
-		return cal1 - cal2 - cal3;
-		
-	}
-	
-	public static int multicalc(int cal1, int cal2,  int cal3) {
-		return cal1 * cal2 * cal3;
+	public static int multicalc(int cal1, int cal2) {
+		return cal1 * cal2 ;
 		
 	} 
-	public static int divcalc(int cal1, int cal2,  int cal3) {
-		return cal1 / cal2 / cal3;
+	public static int divcalc(int cal1, int cal2) {
+		return cal1 / cal2;
 		
 	}
 	
--- Calculator/src/com/qa/calculator/Runner.java
+++ Calculator/src/com/qa/calculator/Runner.java
@@ -1,16 +1,71 @@
 package com.qa.calculator;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Runner {
 public static void main(String[] args) {
-	int cal1 = 56;
-	int cal2 = 7;
-	int cal3 = 10;
 	
-	System.out.println(CalcMethods.addcalc(cal1, cal2, cal3 ));
-	System.out.println(CalcMethods.minuscalc(cal1, cal2, cal3 ));
-	System.out.println(CalcMethods.multicalc(cal1, cal2, cal3 ));
-	System.out.println(CalcMethods.divcalc(cal1, cal2, cal3 ));
-	System.out.println(CalcMethods.modcalc(cal1, cal2 ));
+	
+	
+	 Scanner scanner1  = new Scanner(System.in);
+	 CalcMethods method = new CalcMethods();
+	 
+	 String operator;
+	 
+	 try {
+	 
+	 System.out.println("please input your first number");
+	
+	 int cal1 = scanner1.nextInt();
+	 
+	 Scanner scanner2  = new Scanner(System.in);
+	 System.out.println("please enter your second number ");
+	
+	 int cal2 = scanner2.nextInt();
+	 
+	 Scanner scanner3  = new Scanner(System.in);
+	 System.out.println("Please enter your operator ");
+	 operator = scanner3.nextLine();
+	
+	 
+	 switch(operator) {
+	 
+	 case "+":
+		 System.out.println(CalcMethods.addcalc(cal1, cal2 ));
+		 break;
+	 case "-":
+		 System.out.println(CalcMethods.minuscalc(cal1, cal2 ));
+		 break;
+	 case"/":
+		 System.out.println(CalcMethods.multicalc(cal1, cal2  ));
+	     break;
+	 case "*":
+		 System.out.println(CalcMethods.divcalc(cal1, cal2  ));
+		 break;
+	 case"%":
+		 System.out.println(CalcMethods.modcalc(cal1, cal2 ));
+		 break;
+	default:
+		System.out.println("default");
+		break;
+		 
+	 }} catch(InputMismatchException e){
+		 System.out.println("please try again and enter a valid operator");
+	 }
+	
+	
+	 
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 }
 }
